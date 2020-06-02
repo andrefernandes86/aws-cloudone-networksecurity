a32a8a06f3d246c729134a7550ad22dbc0a66614
Andre Fernandes <andre.fernandes86@gmail.com>
2020-06-02 14:19:28 +0400

    Update ec2_ubuntu_attacker.sh

--- ec2_ubuntu_attacker.sh
+++ ec2_ubuntu_attacker.sh
@@ -9,8 +9,5 @@ sleep 10
 sudo usermod -aG docker root
 sleep 5
 
-# Installing DSA
-DSA Script
-
 sleep 45
 sudo docker run -d --rm --name demo-dvwa -d -p 8080:80 andrefernandes86/demo-apachestruts-attacker-web
