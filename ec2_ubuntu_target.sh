fce50d116841050e3c15dee8ea9d7763e04aff12
Andre Fernandes <andre.fernandes86@gmail.com>
2020-06-02 14:19:10 +0400

    Create ec2_ubuntu_target.sh

---
+++ ec2_ubuntu_target.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo su
+sudo apt-get update
+sleep 10
+sudo apt-get install python -y
+sleep 10
+curl -fsSL https://get.docker.com | sh
+sleep 10
+sudo usermod -aG docker root
+sleep 5
+
+# Creating the vulnerable container
+sleep 45
+sudo docker run -d --rm --name demo-apachestruts-target -d -p 80:8080 andrefernandes86/demo-apachestruts-target
